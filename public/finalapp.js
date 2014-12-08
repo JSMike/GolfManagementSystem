1740240ad4553c3a57adfee2d4ba0b2aac05630c
Michael Cebrian <mcebri01@villanova.edu>
2014-12-08 02:36:51 -0500

    scope test

--- public/finalapp.js
+++ public/finalapp.js
@@ -3,204 +3,140 @@ var finalApp = angular.module('finalApp', ['ngRoute']);
 finalApp.controller('finalController', function($scope, $http) {
   $scope.db = {};
   // Clubs
-  $http.get('/api/clubs')
-  .success(function(data) {
-    $scope.db.clubs = data;
-    console.log('Clubs: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.clubs =  [];
-  });
   $scope.Clubs = function() {
     $http.get('/api/clubs')
     .success(function(data) {
       $scope.db.clubs = data;
       console.log('Clubs: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.clubs =  [];
+      $scope.$apply();
     });
   };
 
   // Courses
-  $http.get('/api/courses')
-  .success(function(data) {
-    $scope.db.courses =  data;
-    console.log('Courses: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.courses = [];
-  });
   $scope.Courses = function() {
     $http.get('/api/courses')
     .success(function(data) {
       $scope.db.courses =  data;
       console.log('Courses: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.courses = [];
+      $scope.$apply();
     });
   };
 
   // Holes
-  $http.get('/api/holes')
-  .success(function(data) {
-    $scope.db.holes = data;
-    console.log('Holes: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.holes = [];
-  });
   $scope.Holes = function() {
     $http.get('/api/holes')
     .success(function(data) {
       $scope.db.holes = data;
       console.log('Holes: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.holes = [];
+      $scope.$apply();
     });
   };
 
   // Matchups
-  $http.get('/api/matchups')
-  .success(function(data) {
-    $scope.db.matchups = data;
-    console.log('Matchups: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.matchups = [];
-  });
   $scope.Matchups = function() {
     $http.get('/api/matchups')
     .success(function(data) {
       $scope.db.matchups = data;
       console.log('Matchups: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.matchups = [];
+      $scope.$apply();
     });
   };
 
   // Players
-  $http.get('/api/players')
-  .success(function(data) {
-    $scope.db.players = data;
-    console.log('Players: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.players = [];
-  });
   $scope.Players = function() {
     $http.get('/api/players')
     .success(function(data) {
       $scope.db.players = data;
       console.log('Players: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.players = [];
+      $scope.$apply();
     });
   };
 
   // Rounds
-  $http.get('/api/rounds')
-  .success(function(data) {
-    $scope.db.rounds = data;
-    console.log('Rounds: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.rounds = [];
-  });
   $scope.Rounds = function() {
     $http.get('/api/rounds')
     .success(function(data) {
       $scope.db.rounds = data;
       console.log('Rounds: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.rounds = [];
+      $scope.$apply();
     });
   };
 
   // Scores
-  $http.get('/api/scores')
-  .success(function(data) {
-    $scope.db.scores = data;
-    console.log('Scores: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.scores = [];
-  });
   $scope.Scores = function() {
     $http.get('/api/scores')
     .success(function(data) {
       $scope.db.scores = data;
       console.log('Scores: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.scores = [];
+      $scope.$apply();
     });
   };
 
   // Seasons
-  $http.get('/api/seasons')
-  .success(function(data) {
-    $scope.db.seasons = data;
-    console.log('Seasons: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.seasons = [];
-  });
   $scope.Seasons = function() {
     $http.get('/api/seasons')
     .success(function(data) {
       $scope.db.seasons = data;
       console.log('Seasons: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.seasons = [];
+      $scope.$apply();
     });
   };
 
   // Weeks
-  $http.get('/api/weeks')
-  .success(function(data) {
-    $scope.db.weeks = data;
-    console.log('Weeks: ' + data);
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-    $scope.db.weeks = [];
-  });
   $scope.Weeks = function() {
     $http.get('/api/weeks')
     .success(function(data) {
       $scope.db.weeks = data;
       console.log('Weeks: ' + data);
+      $scope.$apply();
     })
     .error(function(data) {
       console.log('Error: ' + data);
       $scope.db.weeks = [];
+      $scope.$apply();
     });
   };
 
-  $scope.scopeTest = "Testing123";
   $scope.init = function () {
     console.log("Calling functions.");
     $scope.Clubs();
@@ -213,7 +149,7 @@ finalApp.controller('finalController', function($scope, $http) {
     $scope.Seasons();
     $scope.Teams();
     $scope.Weeks();
-    return "";
+    $scope.$apply();
   };
 
 });
